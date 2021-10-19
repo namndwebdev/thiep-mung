7659cfd2a6e3c114202e74de6ba046ea3095f5b6
Đình Nam <namndwebdev@gmail.com>
2021-10-19 17:34:27 +0700

    update config code

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Chúc người ấy';
+const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const giftImage = 'hot-girl.png';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Nhóm zalo tự học lập trình: https://zalo.me/g/yhdkef092
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
--- present.js
+++ present.js
@@ -1,16 +1,23 @@
 
-var to = 'Em!';
-var gift_url = 'http://nodemy.vn';
-var gift_image_url = 'hot-girl.png';
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Nhóm zalo tự học lập trình: https://zalo.me/g/yhdkef092
 
+var to = nameGirl;
+var gift_url = giftUrl;
+var gift_image_url = giftImage || giftImageBase64;
 
 var nametag = document.getElementById("nametag");
 var present = document.getElementById("present");
 var presentImage = document.getElementById("present-image");
 
-
 function init() {
-  
+  var graphElem = document.querySelector('.present-box > .side.top .to');
+  graphElem.setAttribute('data-before', eventName);
+  document.querySelector('#card .title-card').innerHTML = `💘${titleCard}💘`;
+  document.querySelector('#card .content-card').innerHTML = `${contentCard}`;
+  document.querySelector('#card .honey').setAttribute('src', `${giftImage}`);
+
   var _giftLink, 
       _giftImg;
   
