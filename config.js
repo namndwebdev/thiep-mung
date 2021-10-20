1ce234a020728321364396299f4e98ecfc35be15
GTTLXDxNHT1722003 <92871720+GTTLXDxNHT1722003@users.noreply.github.com>
2021-10-20 22:18:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ const contentCard = 'Chúc Trúc của anh 20/10 tràn ngập niềm vui và nh�
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'hot-girl.png';
+const giftImage = 'hot-girl.png.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
