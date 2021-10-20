5d0c175ca1e19c229494fd7611f01f9862a605c3
GTTLXDxNHT1722003 <92871720+GTTLXDxNHT1722003@users.noreply.github.com>
2021-10-20 23:10:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,12 @@
-const nameGirl = 'Trúc';
-const giftUrl = 'http://nodemy.vn';
+const nameGirl = 'T.Uyên';
 const eventName = 'Chúc Mừng 20-10';
 const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc Trúc của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em bây giờ và mãi mãi ';
+const contentCard = 'Chúc Uyên ,  20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bạn bây giờ và mãi mãi ';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'hot-girl.png.jpg';
+const giftImage = 'Uyên-_-';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
