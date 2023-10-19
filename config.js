dbde3932aca5288411b017ea0ba15e5a3c7e6ad5
tien09101017 <86121914+tien09101017@users.noreply.github.com>
2023-10-19 23:34:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-const nameGirl = 'Chy Lợn';
+const nameGirl = 'En';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng 20-10';
-const titleCard = 'Tặng Chy Lợn';
+const titleCard = 'Tặng Em';
 const contentCard = 'Chúc em 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
